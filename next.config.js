4d335544b11900dbf088d6e0ce2929da008288fe
Nsairun <emmansairun@gmail.com>
2024-02-08 07:05:28 +0100

    added a config module

--- next.config.js
+++ next.config.js
@@ -2,3 +2,10 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
+
+module.exports = {
+    webpack: (config) => {
+      config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+      return config;
+    },
+  };
